87cc7c78561655e695b36252028efe814bc808a5
Regan Tosh <toshregan94@gmail.com>
2026-04-24 23:55:39 +0300

    changes

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
-  const color="light grey";
+  const color="lightgray";
   document.body.style.backgroundColor=color;
-  
-  
+ 
 }
 
 // Function to reset the background color when the body is double-clicked
@@ -21,7 +20,11 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
-  document.getElementById("keyPressDisplay").textContent= "You pressed: ${event.key}";
+  const display=document.getElementById("keyPressDisplay");
+  if(display){
+    display.textContent="You pressed: ${event.key}";
+  }
+
 }
 
 // Process Text Input
@@ -31,19 +34,34 @@ function displayUserInput() {
   // Implement the function to display user input
   const input=document.getElementById("textInput").value;
   document.getElementById("textInputDisplay").textContent=input;
+  const display=document.getElementById("textInputDisplay");
+  if (display){
+    display.textContent= input;
+  }
 }
 
 // Attach Event Listeners
 function setupEventListeners() {
 // Attach event listener to change background color when the button is clicked
+const colorBtn=document.getElementById("changeColorButton");
+
+
   document
     .getElementById('changeColorButton')
     .addEventListener('click', changeBackgroundColor)
+    if (colorBtn){
+  colorBtn.addEventListener("click", changeBackgroundColor)
+    }
+  
 
   // Attach event listener to reset background color when the body is double-clicked
+  const resetBtn=document.getElementById("resetColorButton")
   document
     .getElementById('resetColorButton')
     .addEventListener('dblclick', resetBackgroundColor)
+    if (resetBtn){
+      resetBtn.addEventListener("dblclick", resetBackgroundColor)
+    }
 
   // Attach event listener to display key pressed when a key is pressed down
   document.addEventListener('keydown', displayKeyPress)
