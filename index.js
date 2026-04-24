3244dc363fbd90e96f7acce8673252d8c4d6fa19
Regan Tosh <toshregan94@gmail.com>
2026-04-24 23:34:34 +0300

    New

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  const color="light grey";
+  document.body.style.backgroundColor=color;
+  
+  
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  document.body.style.backgroundColor="pink";
 }
 
 // Capture Keyboard Input
 
+
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  document.getElementById("keyPressDisplay").textContent= "You pressed: ${event.key}";
 }
 
 // Process Text Input
@@ -22,6 +29,8 @@ function displayKeyPress(event) {
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  const input=document.getElementById("textInput").value;
+  document.getElementById("textInputDisplay").textContent=input;
 }
 
 // Attach Event Listeners
